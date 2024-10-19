caca7482eb948cb7337655c569777860b5dd6923
Jeffrey <66732562+JeffreyLee143@users.noreply.github.com>
2024-10-19 13:27:57 +0800

    HW1_BMI

---
+++ Models/BMI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPP_BMI.Models
+{
+    public class BMI
+    {
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string Caculate_BMI()
+        {
+            double b;
+            string r="Your BMI: ";
+            if (Height >= 2) Height /= 100;
+            if (Weight <= 0) throw new Exception();
+            b= Math.Round(Weight / (Height * Height),2);
+            r += b+"<br>";
+            if (b < 18.5)
+            {
+                r += "體重過輕";
+            }else if (b < 24)
+            {
+                r += "正常範圍";
+            }else if (b < 27)
+            {
+                r += "過重";
+            }else if (b < 30)
+            {
+                r += "輕度肥胖";
+            }else if (b < 35)
+            {
+                r += "中度肥胖";
+            }
+            else
+            {
+                r += "重度肥胖";
+            }
+            return r;
+        }
+        public BMI(double height, double weight)
+        {
+            Height = height;
+            Weight = weight;
+        }
+    }
+}
