564ab9587157cd14e99b181e25a10a44cea11238
Jeffrey <66732562+JeffreyLee143@users.noreply.github.com>
2024-12-09 12:01:10 +0800

    2024/12/09

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+namespace WebApplication2.Data
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<student> student { get; set; }
+        
+    }
+}
--- Models/student.cs
+++ Models/student.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication2.Models;
 
-namespace WebApplication1.Models
+namespace WebApplication2.Models
 {
     public class student
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public int Name { get; set; }
     }
 }
