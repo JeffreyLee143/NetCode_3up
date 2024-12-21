2d7198cd4b3fbc537062db9231c306708fac5a86
Jeffrey <66732562+JeffreyLee143@users.noreply.github.com>
2024-12-21 15:44:51 +0800

    資料庫

---
+++ dbo.Task.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Task] (
+    [TaskId]      INT          NOT NULL,
+    [Title]       VARCHAR (20) NOT NULL,
+    [Description] VARCHAR (50) NOT NULL,
+    [Budget]      INT          NOT NULL,
+    [DueDate]     DATETIME     NOT NULL,
+    PRIMARY KEY CLUSTERED ([TaskId] ASC)
+);
+
