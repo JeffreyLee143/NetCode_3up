87b387cfd2685da3a4c8e65dd480af1757a5def6
Jeffrey <66732562+JeffreyLee143@users.noreply.github.com>
2024-12-21 15:46:12 +0800

    Add files via upload

---
+++ Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_HW.Models
+{
+    public class Task
+    {
+        public int TaskId {  get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Budget { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
