e0b9e3681b3c26dad028250bdeceda7b3f8688b4
Jeffrey <66732562+JeffreyLee143@users.noreply.github.com>
2024-12-22 16:43:05 +0800

    Update dbo.Task.sql

--- dbo.Task.sql
+++ dbo.Task.sql
@@ -1,9 +1,8 @@
-﻿CREATE TABLE [dbo].[Task] (
-    [TaskId]      INT          NOT NULL,
+CREATE TABLE [dbo].[Task] (
+    [TaskId]      INT           identity(1,1) NOT NULL,
     [Title]       VARCHAR (20) NOT NULL,
     [Description] VARCHAR (50) NOT NULL,
     [Budget]      INT          NOT NULL,
-    [DueDate]     DATETIME     NOT NULL,
-    PRIMARY KEY CLUSTERED ([TaskId] ASC)
+    [DueDate]     DATETIME     NOT NULL, 
+    CONSTRAINT [PK_Task] PRIMARY KEY ([TaskId]),
 );
-
